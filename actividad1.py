3e9827b91765dbeefb280f7af11df59369213358
paolavirginiacastillo <a01784854@tec.mx>
2024-09-12 18:50:48 -0600

    cambios

--- actividad1.py
+++ actividad1.py
@@ -40,7 +40,7 @@ def circle(start, end):
     down()
     radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5 #radio = distancia puntos de inicio y final
     begin_fill()
-    turtle.circle(radius)
+    circle(radius)
     end_fill() #final
 
 def rectangle(start, end):
@@ -113,106 +113,7 @@ onkey(lambda: store('shape', triangle), 't')
 onkey(lambda: store('shape', triangle), 't')
 done()
 
-# Actividad 2. Juego de la Víbora
-
-
-
-# Actividad 3. Juego del Packman
-
-
-
-# Actividad 4. Juego del Tiro Parabólico
-
-
-
-# Actividad 5. Juego de Memoria
-from random import *
-from turtle import *
-
-from freegames import path
-
-car = path('car.gif')
-tiles = list(range(32)) * 2
-state = {'mark': None}
-hide = [True] * 64
-
-
-def square(x, y):
-    """Draw white square with black outline at (x, y)."""
-    up()
-    goto(x, y)
-    down()
-    color('black', 'white')
-    begin_fill()
-    for count in range(4):
-        forward(50)
-        left(90)
-    end_fill()
-
-
-def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
-
-def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
-
-
-def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
-    spot = index(x, y)
-    mark = state['mark']
-
-    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
-        state['mark'] = spot
-    else:
-        hide[spot] = False
-        hide[mark] = False
-        state['mark'] = None
-
-
-def draw():
-    """Draw image and tiles."""
-    clear()
-    goto(0, 0)
-    shape(car)
-    stamp()
-
-    for count in range(64):
-        if hide[count]:
-            x, y = xy(count)
-            square(x, y)
-
-    mark = state['mark']
-
-    if mark is not None and hide[mark]:
-        x, y = xy(mark)
-        up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
-
-    update()
-    ontimer(draw, 100)
-
-
-shuffle(tiles)
-setup(420, 420, 370, 0)
-addshape(car)
-hideturtle()
-tracer(False)
-onscreenclick(tap)
-draw()
-done()
-
-
 """
     Referencias de código implementados:
     1.-https://grantjenks.com/docs/freegames/paint.html
-    2.-http://www.grantjenks.com/docs/freegames/snake.html
-    3.-http://www.grantjenks.com/docs/freegames/pacman.html
-    4.-http://www.grantjenks.com/docs/freegames/cannon.html
-    5.-https://grantjenks.com/docs/freegames/memory.html
 """ 
